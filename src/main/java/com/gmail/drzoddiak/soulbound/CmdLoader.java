ce87ae6d4ef9fa477bdbbb81f43a91bd1471583a
Bruce Linington <DrZoddiak@gmail.com>
2017-08-11 17:03:32 -0500

    Add files via upload

---
+++ src/main/java/com/gmail/drzoddiak/soulbound/CmdLoader.java
@@ -0,0 +1,163 @@
+package com.gmail.drzoddiak.soulbound;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.type.HandTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+public class CmdLoader
+{
+    private CommandSpec addList = CommandSpec.builder()
+            .description(Text.of("")).executor( this::add_list ).arguments(GenericArguments.onlyOne(GenericArguments
+            .string(Text.of("id")))).permission(Reference.ADD_LIST).build();
+
+    private CommandSpec removeList = CommandSpec.builder()
+    		.description(Text.of("")).executor( this::remove_list ).arguments(GenericArguments.onlyOne(GenericArguments
+    		.string(Text.of("id")))).permission(Reference.REMOVE_LIST).build();
+        
+    private CommandSpec addSB = CommandSpec.builder()
+            .description(Text.of("")).executor(this::add_sb).permission(Reference.ADD_SB).build();
+
+    private CommandSpec removeSB = CommandSpec.builder()
+                .description(Text.of("")).executor(this::remove_sb).permission(Reference.REMOVE_SB).build();
+
+            //Base Command for above commands, as commands are added, create additional children
+
+        public CommandSpec sb = CommandSpec.builder()
+            .description(Text.of("Base command")).executor(this::help).permission(Reference.HELP)
+                .child(addList, "add")
+                .child(removeList, "remove")
+                .child(addSB, "addSB")
+                .child(removeSB, "removeSB")
+                .build();
+        
+        public CommandResult help(CommandSource src, CommandContext args) throws CommandException
+        {		
+        	//triggered when they type only the base cmd /sb
+        	return CommandResult.success();
+        }
+        
+        public CommandResult add_list(CommandSource src, CommandContext args) throws CommandException
+        {
+        	if(src instanceof Player)
+        	{
+        		Player player = (Player) src;
+				String arg = args.getOne("id").get().toString();
+        		
+        		if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent() && !arg.equals(""))
+        		{
+        			String id = Reference.getID(player.getItemInHand(HandTypes.MAIN_HAND).get()); 
+        			
+        			if(arg.equalsIgnoreCase("pickup"))
+        			{
+        				if( Reference.addToList(id, 0) )
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully added ", TextColors.WHITE, id, TextColors.GREEN," to the 'pickup' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," Already exists in the 'pickup' list!"));
+        			}
+        			else if(arg.equalsIgnoreCase("use"))
+        			{
+        				if(Reference.addToList(id, 1))
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully added ", TextColors.WHITE, id, TextColors.GREEN," to 'use' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," Already exists in the 'use' list!"));
+        			}
+        			else if(arg.equalsIgnoreCase("equip"))
+        			{
+        				if(Reference.addToList(id, 2))
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully added ", TextColors.WHITE, id, TextColors.GREEN," to the 'equip' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," Already exists in the 'equip' list!"));
+        			}
+        		}
+        			
+        	}
+        	return CommandResult.success();
+        }
+        
+        public CommandResult remove_list(CommandSource src, CommandContext args) throws CommandException
+        {
+        	if(src instanceof Player)
+        	{
+        		Player player = (Player) src;
+        		String arg = args.getOne("id").get().toString();
+        		
+        		if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent() && !arg.equals(""))
+        		{
+        			String id = Reference.getID(player.getItemInHand(HandTypes.MAIN_HAND).get());
+        			
+        			if(arg.equalsIgnoreCase("pickup"))
+        			{
+        				if(Reference.removeFromList(id, 0))
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully removed ", TextColors.WHITE, id, TextColors.GREEN," from the 'pickup' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," Doesn't exist in the 'pickup' list!"));
+        			}
+        			
+        			if(arg.equalsIgnoreCase("use"))
+        			{
+        				if(Reference.removeFromList(id, 1))
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully removed ", TextColors.WHITE, id, TextColors.GREEN," from 'use' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," Doesn't exist in the 'use' list!"));
+        			}
+        			
+        			if(arg.equalsIgnoreCase("equip"))
+        			{
+        				if(Reference.removeFromList(id, 2))
+        					src.sendMessage(Text.of(TextColors.GREEN,"Successfully removed ", TextColors.WHITE, id, TextColors.GREEN," from the 'equip' list!"));
+        				else
+        					src.sendMessage(Text.of(id,TextColors.RED," doesn't exist in the 'equip' list!"));
+        			}
+        			
+        		}
+        			
+        	}
+        	return CommandResult.success();
+        }
+        
+        public CommandResult add_sb(CommandSource src, CommandContext args) throws CommandException
+        {
+        	if(src instanceof Player)
+        	{
+        		Player player = (Player) src;
+        		
+        		if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent())
+        		{
+        			ItemStack stack = player.getItemInHand(HandTypes.MAIN_HAND).get();
+        			List<Text> loreList = new ArrayList<>();
+        			loreList.add(Text.of("Bounded to: "+player.getName()));
+        			loreList.add(Text.of("UUID: "+player.getUniqueId()));
+        			stack.offer(Keys.ITEM_LORE, loreList);
+        			player.setItemInHand(HandTypes.MAIN_HAND, stack);
+        		}
+        	}
+        	return CommandResult.success();
+        }
+        
+        public CommandResult remove_sb(CommandSource src, CommandContext args) throws CommandException
+        {
+        	if(src instanceof Player)
+        	{
+        		Player player = (Player)src;
+        		
+        		if(player.getItemInHand(HandTypes.MAIN_HAND).isPresent())
+        		{
+        			ItemStack stack = player.getItemInHand(HandTypes.MAIN_HAND).get();
+        		}
+        	}
+        	return CommandResult.success();
+
+        }
+}
---
+++ src/main/java/com/gmail/drzoddiak/soulbound/Reference.java
@@ -0,0 +1,113 @@
+package com.gmail.drzoddiak.soulbound;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.spongepowered.api.data.DataContainer;
+import org.spongepowered.api.data.DataQuery;
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.item.inventory.ItemStack;
+ 
+
+public class Reference 
+{
+//@Plugin data parameters
+	public static final String NAME = "Main";
+	public static final String ID = "soulbound";
+	public static final String VERSION = "0.0.1";
+	public static final String DESC = "Binds items to the users soul!";
+	public static final String AUTHORS = "DrZoddiak & Burpingdog1";
+
+//Permissions
+	//users
+	public static final String PICKUP = "soulbound.user.pickup";
+	public static final String USE = "soulbound.user.use";
+	public static final String EQUIP = "soulbound.user.equip";
+	public static final String KEEP_ON_DEATH = "soulbound.user.keep";
+	//admins
+	public static final String HELP = "soulbound.admin";
+	public static final String ADD_LIST = "soulbound.admin.addlist";
+	public static final String REMOVE_LIST = "soulbound.admin.removelist";
+	public static final String ADD_SB = "soulbound.admin.addsb";
+	public static final String REMOVE_SB = "soulbound.admin.removesb";
+	
+	//Plugin config data
+	public static List<String> sb_pickup = new ArrayList<>();
+	public static List<String> sb_use = new ArrayList<>();
+	public static List<String> sb_equip = new ArrayList<>();
+	public static boolean pickup_perm;
+	public static boolean use_perm;
+	public static boolean equip_perm;
+	public static boolean keep_perm;
+	
+	public static boolean addToList(String id, int index)
+	{
+		switch (index)
+		{
+		case 0: //pick up
+			if(sb_pickup.contains(id))
+				return false;
+			sb_pickup.add(id);
+			SBConfig.savetoFile();
+			return true;
+		case 1: //use
+			if (sb_use.contains(id)) 
+				return false;
+			sb_use.add(id);
+			SBConfig.savetoFile();
+			return true;
+		case 2: //equip
+			if(sb_equip.contains(id))
+				return false;
+			sb_equip.add(id);
+			SBConfig.savetoFile();
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean removeFromList(String id, int index)
+	{
+		switch (index)
+		{
+		case 0: //pickup
+			if(sb_pickup.contains(id))
+			{
+				sb_pickup.remove(id);
+				SBConfig.savetoFile();
+				return true;
+			}
+			return false;
+		case 1://use
+			if(sb_use.contains(id))
+			{
+				sb_use.remove(id);
+				SBConfig.savetoFile();
+				return true;
+			}
+			return false;
+		case 2://equip
+			if(sb_equip.contains(id))
+			{
+				sb_equip.remove(id);
+				SBConfig.savetoFile();
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+
+	public static String getID(ItemStack stack)
+	{
+		String ID = stack.getItem().getId();
+		DataContainer container = stack.toContainer();
+		DataQuery data = DataQuery.of('/', "UnsafeDamage");
+		int meta = Integer.parseInt(container.get(data).get().toString());
+		if(meta != 0 && stack.getValue(Keys.UNBREAKABLE).get().get())
+		{
+			ID = ID+":"+meta;
+		}
+		return ID;
+	}
+}
