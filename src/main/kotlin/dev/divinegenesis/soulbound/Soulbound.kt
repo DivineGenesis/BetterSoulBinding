687b49436961d826603fb5d063703c3266195c2d
Jesse McKee <jessemckee97@gmail.com>
2021-07-14 05:18:47 -0500

    Expand Events in main class

--- src/main/kotlin/dev/divinegenesis/soulbound/Soulbound.kt
+++ src/main/kotlin/dev/divinegenesis/soulbound/Soulbound.kt
@@ -3,18 +3,38 @@ package dev.divinegenesis.soulbound
 import org.apache.logging.log4j.Logger
 import org.spongepowered.plugin.jvm.Plugin
 import com.google.inject.Inject
+import dev.divinegenesis.soulbound.customdata.Data
+import org.spongepowered.api.Engine
 import org.spongepowered.api.Server
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.event.Listener
-import org.spongepowered.api.event.lifecycle.StartedEngineEvent
+import org.spongepowered.api.event.lifecycle.*
 import org.spongepowered.plugin.PluginContainer
 
 
 @Plugin("soulbound")
-class Soulbound @Inject internal constructor(private val container: PluginContainer, private val logger: Logger) {
+data class Soulbound @Inject internal constructor(private val container: PluginContainer, private val logger: Logger) {
 
     @Listener
-    fun onStart (event: StartedEngineEvent<Server>) {
-        logger.info("Server has started with a new thing")
+    fun onPluginConstruction(event: ConstructPluginEvent) {
+        logger.info("Soulbound constructing..")
+        //Config
+
+        Sponge.eventManager().registerListeners(container, Data(container,logger))
+    }
+
+    //@Listener
+    //fun onRegisterCommand(event: RegisterCommandEvent<>)
+
+
+    @Listener
+    fun onReload(event: RefreshGameEvent) {
+        logger.info("Game refreshed...")
+    }
+
+    @Listener
+    fun onShutdown(event: StoppingEngineEvent<Engine>) {
+        logger.info("Server shutting down")
     }
 
 }
