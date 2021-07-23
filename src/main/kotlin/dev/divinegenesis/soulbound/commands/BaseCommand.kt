68c3370b919dd393a3660d6a633a50762a6e55f7
Jesse McKee <jessemckee97@gmail.com>
2021-07-23 09:46:28 -0500

    Add storage command This command allows users to set and unset binding types on items with in-game commands "/sb set [interact/bind/craft] [true/false]"

--- src/main/kotlin/dev/divinegenesis/soulbound/commands/BaseCommand.kt
+++ src/main/kotlin/dev/divinegenesis/soulbound/commands/BaseCommand.kt
@@ -1,19 +1,43 @@
 package dev.divinegenesis.soulbound.commands
 
 import dev.divinegenesis.soulbound.Utils
+import dev.divinegenesis.soulbound.logger
+import dev.divinegenesis.soulbound.storage.DataStack
+import dev.divinegenesis.soulbound.storage.SqliteDatabase
 import net.kyori.adventure.text.Component
+import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.Command.*
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.parameter.CommandContext
 import org.spongepowered.api.command.exception.CommandException
+import org.spongepowered.api.command.parameter.CommonParameters
+import org.spongepowered.api.command.parameter.Parameter
 import org.spongepowered.api.data.type.HandTypes
 import org.spongepowered.api.entity.living.player.server.ServerPlayer
+import org.spongepowered.api.item.ItemTypes
+import org.spongepowered.api.registry.RegistryTypes
 
 
 class BaseCommand {
+
+
+    enum class Choices {
+        INTERACT,
+        CRAFT,
+        PICKUP
+    }
+
+    private val enumParameter = Parameter.enumValue(Choices::class.java).key("enum").build()
+
     private val bindCommand = builder()
         .shortDescription(Component.text("Binds the item to the player holding it"))
         .executor(this::bindCommandResult)
+        .permission("")
+        .build()
+    private val setStorage = builder()
+        .shortDescription(Component.text("Adds the item to the database!"))
+        .executor(this::storageCommandResult)
+        .addParameter(Parameter.seq(enumParameter, CommonParameters.BOOLEAN))
         .permission("")
         .build()
 
@@ -23,6 +47,7 @@ class BaseCommand {
         .executor(this::helpCommandResult)
         .permission("")
         .addChild(bindCommand, "bind")
+        .addChild(setStorage, "set","add")
         .build()
 
     @Throws(CommandException::class)
@@ -41,7 +66,38 @@ class BaseCommand {
                 )
             )
         }
+        return CommandResult.success()
+    }
 
+    @Throws(CommandException::class)
+    private fun storageCommandResult(context: CommandContext): CommandResult {
+
+        val sender = context.cause().root()
+        val sql = SqliteDatabase()
+
+        if (sender is ServerPlayer) {
+            val itemType = sender.itemInHand(HandTypes.MAIN_HAND).type()
+            if (itemType.isAnyOf(ItemTypes.AIR)) {
+                return CommandResult.error(Component.text("You need to have an item in your main hand!"))
+            }
+            val itemID = Sponge.game().registries().registry(RegistryTypes.ITEM_TYPE).valueKey(itemType).formatted()
+            val dataStack = sql.loadData()[itemID] ?: DataStack(itemID)
+            val checkInteraction = context.one(CommonParameters.BOOLEAN).get()
+
+            when (context.one(enumParameter).get()) {
+                Choices.INTERACT -> {
+                    dataStack.interact = checkInteraction.toInt()
+                }
+                Choices.CRAFT -> {
+                    dataStack.craft = checkInteraction.toInt()
+                }
+                Choices.PICKUP -> {
+                    dataStack.pickup = checkInteraction.toInt()
+                }
+            }
+            sql.saveStack(dataStack)
+            sender.sendMessage(Component.text("${sql.loadData().size} entries"))
+        }
         return CommandResult.success()
     }
 
@@ -53,12 +109,15 @@ class BaseCommand {
 
         if (sender is ServerPlayer) {
             val itemStack = sender.itemInHand(HandTypes.MAIN_HAND)
+            if (itemStack.type().isAnyOf(ItemTypes.AIR)) {
+                return CommandResult.error(Component.text("You need to have an item in your main hand!"))
+            }
             val finalStack = Utils().sortData(itemStack, sender.uniqueId()).first
-
-            sender.setItemInHand(HandTypes.MAIN_HAND,finalStack)
+            sender.setItemInHand(HandTypes.MAIN_HAND, finalStack)
         }
-
         return CommandResult.success()
     }
 }
+
+fun Boolean.toInt() = if (this) 1 else 0
 
