61ebce95900c5dd64ee197cd6ff6c9225fc19304
Jesse McKee <jessemckee97@gmail.com>
2021-07-23 22:30:36 -0500

    Formatting

--- src/main/kotlin/dev/divinegenesis/soulbound/config/Config.kt
+++ src/main/kotlin/dev/divinegenesis/soulbound/config/Config.kt
@@ -7,15 +7,17 @@ import org.spongepowered.configurate.objectmapping.meta.Setting
 @ConfigSerializable
 class Config {
     @Setting("Modules")
-    val modules : Modules = Modules()
+    val modules: Modules = Modules()
 }
 
 @ConfigSerializable
 class Modules {
     @Setting("Interact-Item")
     val interactItemModule = true
+
     @Setting("Pickup-Item")
     val pickupItemModule = true
+
     @Setting("Craft-Item")
     val craftItemModule = true
 }
