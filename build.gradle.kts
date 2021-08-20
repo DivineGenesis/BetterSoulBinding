2e9df028eefc20d1a1575cb90dc022a37009ef1a
Jesse McKee <jessemckee97@gmail.com>
2021-08-19 22:45:05 -0500

    add Permissions

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ sponge {
         displayName("Soulbound")
         mainClass("dev.divinegenesis.soulbound.Soulbound")
         description("Bind items to users soul")
+        version("0.1.0")
         links {
             homepage("https://github.com/DivineGenesis/BetterSoulBinding")
             source("https://github.com/DivineGenesis/BetterSoulBinding")
